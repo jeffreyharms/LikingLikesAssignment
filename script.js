cd3d6d408db7974b87bf966ae68ff494b4b9df0e
Jeff Harms <jeffdharms@gmail.com>
2022-05-24 15:33:20 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function countUP1() {
+    document.querySelector("#likeCount1").innerText++;
+}
+
+function countUP2() {
+    document.querySelector("#likeCount2").innerText++;
+}
+
+function countUP3() {
+    document.querySelector("#likeCount3").innerText++;
+}
